f7d0778d8a76273551bf0ecc19a55b30a5741a85
LuciaRyan <lcryan06@gmail.com>
2023-06-16 11:36:20 +0100

    list still has to be inst. properly - check stack overflow / edhub

--- src/PokemonGym.java
+++ src/PokemonGym.java
@@ -1,6 +1,8 @@
 public interface PokemonGym {
     void enteredTheGym(PokemonTrainer player1);
 
+    void getPokemons();
+
     //TODO : as this is the interface, there must be all methods in here - right ? What are the exact methods ?
 
 }
