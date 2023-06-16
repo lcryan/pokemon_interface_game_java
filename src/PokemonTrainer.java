743481eb3bc3153f05c1336939c2ea1e9f32e507
LuciaRyan <lcryan06@gmail.com>
2023-06-16 15:15:56 +0100

    not quite sure about the solution for list here - instatiation - how to ?

--- src/PokemonTrainer.java
+++ src/PokemonTrainer.java
@@ -3,21 +3,16 @@ import java.util.List;
 public class PokemonTrainer {
     String name;
     String user;
-    String pokemons;
 
     public PokemonTrainer(String name, List<Pokemon> pokemons) {
         this.name = name;
         this.user = user;
-        this.pokemons = pokemons.toString();
 
-        // TODO : the names of string have to change here! To be researched on !
+        // TODO : the names of string have to change here! To be researched on ! //
     }
 
     public String getName() {
         return name;
     }
 
-    public List<Pokemon> getPokemons() {
-        return pokemons.toString();
-    }
 }
