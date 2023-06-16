19a788b25cc39a4c9a14d368cbdb16b86fda4b2c
LuciaRyan <lcryan06@gmail.com>
2023-06-16 15:12:57 +0100

    implemented all methods so far - not quite sure aboutr int randomAttackByGymOwner or String chooseAttackplayer - tbd

--- src/PokemonGym.java
+++ src/PokemonGym.java
@@ -1,8 +1,29 @@
+import java.util.List;
+
 public interface PokemonGym {
     void enteredTheGym(PokemonTrainer player1);
 
-    void getPokemons();
+    void printPokemon(List<Pokemon> pokemons);
 
-    //TODO : as this is the interface, there must be all methods in here - right ? What are the exact methods ?
+    Pokemon selectPokemon(String pokemon, PokemonTrainer trainer);
+
+    void fightRound(PokemonTrainer trainer, PokemonGymOwner owner, Pokemon pokemon, Pokemon gymPokemon);
+
+    Pokemon chooseGymPokemon(PokemonGymOwner gymOwner);
+
+    Pokemon choosePokemon(PokemonTrainer trainer);
+
+    int randomAttackByGymOwner();
+
+    String chooseAttackPlayer(Pokemon p);
+
+    void performAttackPlayer(Pokemon pokemon, Pokemon gymPokemon, String attack);
+
+    void gymOwnerAttacks(Pokemon gymPokemon, Pokemon pokemon);
+
+    void attackOrChange(Pokemon pokemon, Pokemon gymPokemon, PokemonTrainer trainer, PokemonGymOwner gym);
+
+
+    //TODO : as this is the interface, there must be all methods in here - right ? What are the exact methods Check PokemonGymImpl?
 
 }
