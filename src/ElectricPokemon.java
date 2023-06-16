aed9202a2441a0cbb379de849becbe417ed9220e
LuciaRyan <lcryan06@gmail.com>
2023-06-16 15:55:59 +0100

    all methods for electric pokemon implemented and deleted  out of methods.java

--- src/ElectricPokemon.java
+++ src/ElectricPokemon.java
@@ -3,4 +3,16 @@ public class ElectricPokemon extends Pokemon {
     public ElectricPokemon(String name, int level, int hp, String food, String sound) {
         super(name, level, hp, food, sound);
     }
+
+    void thunderPunch(Pokemon name, Pokemon enemy) {
+    }
+
+    void electroBall(Pokemon name, Pokemon enemy) {
+    }
+
+    void thunder(Pokemon name, Pokemon enemy) {
+    }
+
+    void voltTackle(Pokemon name, Pokemon enemy) {
+    }
 }
