2b3acf7bc54e43186d9d4d972b77fb3808ea56a9
LuciaRyan <lcryan06@gmail.com>
2023-06-16 15:23:40 +0100

    error for type from Pokemon super class - should be implemented - but then gives an error if implemented

--- src/GrassPokemon.java
+++ src/GrassPokemon.java
@@ -1,5 +1,5 @@
 public class GrassPokemon extends Pokemon {
     public GrassPokemon(String name, int level, int hp, String food, String sound) {
-        super(name, level, hp, food, sound);
+        super(name, level, hp, food, sound, type);
     }
 }
