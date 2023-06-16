7def5ec21f91e3186e693001314121bd5c155b7a
LuciaRyan <lcryan06@gmail.com>
2023-06-16 15:48:23 +0100

    type as a variable still has to be solved

--- src/FirePokemon.java
+++ src/FirePokemon.java
@@ -6,6 +6,7 @@ public class FirePokemon extends Pokemon {
 
     public FirePokemon(String name, int level, int hp, String food, String sound) {
         super(name, level, hp, food, sound);
+        //TODO : this has to be solved !
     }
 
     public void inferno(Pokemon pokemon, Pokemon gymPokemon) {
